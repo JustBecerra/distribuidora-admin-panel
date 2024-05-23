fc2bbe102a57a1ecc2fd45735c25b1c0491b8004
JustBecerra <justj.becerra@gmail.com>
2024-05-23 10:24:47 -0300

    add ignore exception

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
-import {
-  Admin,
-  Resource,
-  ListGuesser,
-  EditGuesser,
-  ShowGuesser,
-} from "react-admin";
+import { Admin } from "react-admin";
 import { dataProvider } from "./dataProvider";
 import { authProvider } from "./authProvider";
 
