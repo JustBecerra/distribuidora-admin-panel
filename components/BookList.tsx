4ff0b5be29e77a55242ff22baff311cf667b5495
JustBecerra <justj.becerra@gmail.com>
2024-05-24 16:25:05 -0300

    add crop to book description

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,23 +1,30 @@
 import React from "react";
-import { Datagrid, List, TextField } from "react-admin";
+import { Datagrid, DateField, ImageField, List, TextField } from "react-admin";
 
 export const BookList = () => (
   <List>
     <Datagrid rowClick="edit">
-      <TextField source="id" />
-      <TextField source="isbn" />
-      <TextField source="name" />
-      <TextField source="price" />
-      <TextField source="tags" />
-      <TextField source="available" />
-      <TextField source="total_pages_number" />
-      <TextField source="size" />
-      <TextField source="weight" />
-      <TextField source="language" />
-      <TextField source="publication_date" />
-      <TextField source="description" />
-      <TextField source="recommended" />
-      <TextField source="image" />
+      <TextField source="id" label="ID" />
+      <TextField source="isbn" label="ISBN" />
+      <TextField source="name" label="Título" />
+      <TextField source="price" label="Precio" />
+      <TextField source="tags" label="Genero" />
+      <TextField source="available" label="Unidades Disponibles" />
+      <TextField source="total_pages_number" label="Paginas" />
+      <TextField source="size" label="Tamaño" />
+      <TextField source="weight" label="Peso" />
+      <TextField source="language" label="Idioma" />
+      <DateField source="publication_date" label="Fecha de Publicación" />
+      <TextField
+        source="description"
+        label="Descripción"
+        style={{
+          maxWidth: "50px",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+          whiteSpace: "nowrap",
+        }}
+      />
     </Datagrid>
   </List>
 );
