03a62393984ad3791b60dbb653beecf99f108704
JustBecerra <justj.becerra@gmail.com>
2024-05-28 15:40:22 -0300

    add styles

--- components/CreateForm.tsx
+++ components/CreateForm.tsx
@@ -1,25 +1,73 @@
 import React from "react";
-import { Create, DateInput, SimpleForm, TextInput } from "react-admin";
-
+import { Create, DateInput, Form, SaveButton, TextInput } from "react-admin";
+import { Grid, Typography, Box } from "@mui/material";
 export const CreateForm = () => {
   return (
-    <Create title="Agregar libro" resource="Agregar libro">
-      <SimpleForm>
-        <TextInput source="name" label="Título" />
-        <TextInput source="isbn" label="ISBN" />
-        <TextInput source="price" label="Precio" />
-        <TextInput source="tags" label="Género" />
-        <TextInput source="available" label="Unidades Disponibles" />
-        <TextInput source="total_pages_number" label="Páginas" />
-        <TextInput source="size" label="Tamaño" />
-        <TextInput source="language" label="Idioma" />
-        <DateInput
-          label="Fecha de Publicación"
-          source="publication_date"
-          defaultValue={new Date()}
-        />
-        <TextInput source="description" multiline={true} label="Descripción" />
-      </SimpleForm>
+    <Create title="Agregar Libro" resource="Agregar libro">
+      <Typography fontSize="2rem" ml="1rem" mt="1rem" mb="1rem">
+        Nuevo Libro
+      </Typography>
+      <Box ml="1rem" width="60%">
+        <Form>
+          <Grid
+            container
+            spacing="16"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item xs={6}>
+              <TextInput source="name" label="Título" fullWidth />
+            </Grid>
+            <Grid item xs={6}>
+              <TextInput source="isbn" label="ISBN" fullWidth />
+            </Grid>
+            <Grid item xs={6}>
+              <TextInput source="price" label="Precio" fullWidth />
+            </Grid>
+            <Grid item xs={6}>
+              <TextInput source="tags" label="Género" fullWidth />
+            </Grid>
+            <Grid item xs={6}>
+              <TextInput
+                source="available"
+                label="Unidades Disponibles"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextInput
+                source="total_pages_number"
+                label="Páginas"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextInput source="size" label="Tamaño" fullWidth />
+            </Grid>
+            <Grid item xs={6}>
+              <TextInput source="language" label="Idioma" fullWidth />
+            </Grid>
+            <Grid item xs={6}>
+              <DateInput
+                label="Fecha de Publicación"
+                source="publication_date"
+                defaultValue={new Date()}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextInput
+                source="description"
+                multiline={true}
+                label="Descripción"
+                fullWidth
+              />
+            </Grid>
+          </Grid>
+          <SaveButton style={{ marginBottom: "1rem" }} />
+        </Form>
+      </Box>
     </Create>
   );
 };
