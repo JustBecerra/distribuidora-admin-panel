8e286169dc32a145016285bd36c857319c5f4a32
JustBecerra <justj.becerra@gmail.com>
2024-06-03 12:05:08 -0300

    add optimized query and divider

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,36 @@ import { CreateForm } from "../components/CreateForm";
 import { EditForm } from "../components/EditForm";
 import { i18nProvider } from "./i18Provider";
 import { ShowBook } from "../components/ShowBook";
-export const App = () => (
-  <Admin
-    requireAuth
-    i18nProvider={i18nProvider}
-    dataProvider={customDataProvider}
-    authProvider={authProvider}
-  >
-    <Resource
-      name="librosfiltrados"
-      list={BookList}
-      create={CreateForm}
-      edit={EditForm}
-      show={(props) => (
-        <ShowBook
-          basePath="/librosfiltrados"
-          resource="librosfiltrados"
-          {...props}
-        />
-      )}
-    />
-  </Admin>
-);
+import { QueryClient } from "react-query";
+export const App = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 5 * 60 * 1000, // 5 minutes
+      },
+    },
+  });
+  return (
+    <Admin
+      requireAuth
+      i18nProvider={i18nProvider}
+      dataProvider={customDataProvider}
+      authProvider={authProvider}
+      queryClient={queryClient}
+    >
+      <Resource
+        name="librosfiltrados"
+        list={BookList}
+        create={CreateForm}
+        edit={EditForm}
+        show={(props) => (
+          <ShowBook
+            basePath="/librosfiltrados"
+            resource="librosfiltrados"
+            {...props}
+          />
+        )}
+      />
+    </Admin>
+  );
+};
