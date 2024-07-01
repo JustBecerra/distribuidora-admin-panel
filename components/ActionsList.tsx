a3a1daa8e8ffe76ab86af37c6b072fb716313eb6
JustBecerra <justj.becerra@gmail.com>
2024-07-01 15:52:33 -0300

    delete unnecessary comments

--- components/ActionsList.tsx
+++ components/ActionsList.tsx
@@ -1,19 +1,13 @@
-import React from "react";
 import {
   CreateButton,
   ExportButton,
-  Link,
   SelectColumnsButton,
   TopToolbar,
 } from "react-admin";
-import { Link as RouterLink } from "react-router-dom";
 import SendFile from "./SendFile";
 
 export const ActionsList = () => (
   <TopToolbar sx={{ gap: "0.5rem", display: "flex", alignItems: "center" }}>
-    {/* <Link component={RouterLink} to="/sendfile">
-      Cargar CSV
-    </Link> */}
     <SendFile />
     <SelectColumnsButton />
     <CreateButton />
